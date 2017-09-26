b50b59475bf54d4aaa4b2c811191d7e9ae03a948
hmetsis <32261630+hmetsis@users.noreply.github.com>
2017-09-26 16:37:21 +0200

    Tax by category

--- robot.py
+++ robot.py
@@ -78,27 +78,31 @@ print(robot["price"]*robot["count"]*robot["tax"]+book["price"]*book["count"]*boo
 
 
 
-class Product:
-	def price_with_tax(self):
-		total = self.price * self.count * self.tax
-		if total > 500:
-			return 0.9 * total
-		else:
-			return total
 
-	def __init__(self, price, count, tax):
+class Product:
+	def price_before_tax(self):
+		return self.price * self.count
+
+	def __init__(self, price, count, category):
 		self.price = price
 		self.count = count
-		self.tax = tax
+		self.category = category
 
+	def tax(self):
+		if self.category=="book":
+			return 1.06
+		else:
+			return 1.25
 
+		
 
-products = [Product(price=900,count=2, tax=1.25), Product(price=100, count=1, tax=1.06)]
+products = [Product(price=900,count=2, category="robot"), Product(price=100, count=1, category="book")]
+
 total_price = 0
 for product in products:
-	total_price = total_price + product.price_with_tax()
-print(total_price)
+	total_price = total_price + product.price_before_tax() * product.tax()
 
+print(total_price)
 
 
 
