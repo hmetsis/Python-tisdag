3c61f0897a25fb98fe5b5180d28212e2050b1616
hmetsis <32261630+hmetsis@users.noreply.github.com>
2017-09-26 14:19:53 +0200

    introduced class

--- robot.py
+++ robot.py
@@ -6,6 +6,26 @@ book_amount = 1
 book_tax = 1.06
 print(robot_price * robot_amount * robot_tax + book_price * book_tax)
 
+
+
 robot = {"price": 900, "count": 2, "tax": 1.25}
 book = {"price": 100, "count": 1, "tax": 1.06}
 print(robot["price"]*robot["count"]*robot["tax"]+book["price"]*book["count"]*book["tax"])
+
+
+
+class Product:
+	price = 0
+	count = 0
+	tax = 1.25
+
+robot = Product()
+robot.price = 900
+robot.count = 2
+
+book = Product()
+book.price = 100
+book.count = 1
+book.tax = 1.06
+
+print(robot.price*robot.count*robot.tax+book.price*book.count*book.tax)
