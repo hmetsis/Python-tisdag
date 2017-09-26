934fbb2d5be1d11cd5be91187c8343082e94d0a1
hmetsis <32261630+hmetsis@users.noreply.github.com>
2017-09-26 14:32:12 +0200

    Introduced method

--- robot.py
+++ robot.py
@@ -13,11 +13,31 @@ book = {"price": 100, "count": 1, "tax": 1.06}
 print(robot["price"]*robot["count"]*robot["tax"]+book["price"]*book["count"]*book["tax"])
 
 
-# Class
+# # Class
+# class Product:
+# 	price = 0
+# 	count = 0
+# 	tax = 1.25
+
+# robot = Product()
+# robot.price = 900
+# robot.count = 2
+
+# book = Product()
+# book.price = 100
+# book.count = 1
+# book.tax = 1.06
+
+# print(robot.price*robot.count*robot.tax+book.price*book.count*book.tax)
+
+# Class + method
 class Product:
 	price = 0
 	count = 0
 	tax = 1.25
+
+	def price_with_tax(self):
+		return self.price * self.count * self.tax
 
 robot = Product()
 robot.price = 900
@@ -28,4 +48,4 @@ book.price = 100
 book.count = 1
 book.tax = 1.06
 
-print(robot.price*robot.count*robot.tax+book.price*book.count*book.tax)
+print(robot.price_with_tax()+book.price_with_tax())
