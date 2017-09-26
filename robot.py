411dec5bd52e0876b18ade8f78ddee88dd79d761
hmetsis <32261630+hmetsis@users.noreply.github.com>
2017-09-26 14:01:28 +0200

    introduced all book variables

--- robot.py
+++ robot.py
@@ -1,4 +1,7 @@
 robot_price = 900
 robot_amount = 2
 robot_tax = 1.25
-print(robot_price * robot_amount * robot_tax + 100 * 1.06)
+book_price = 100
+book_amount = 1
+book_tax = 1.06
+print(robot_price * robot_amount * robot_tax + book_price * book_tax)
